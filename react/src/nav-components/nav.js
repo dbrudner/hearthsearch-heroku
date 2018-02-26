035b2bfb9f939346cd5828ba8347b2023c1112f5
dbrudner <dbrudne1@gmail.com>
2018-02-26 17:23:13 -0500

    commit

--- react/src/nav-components/nav.js
+++ react/src/nav-components/nav.js
@@ -150,8 +150,8 @@ export default class Nav extends React.Component {
 						<ul className="nav navbar-nav">
 							<li className=""><Link to="/" className='nav-potato-inverted'>HearthSearch</Link></li>
 							<li className="dropdown">
-								<div className="cursor dropdown-toggle hvr-float" data-toggle="dropdown">Build
-								<span className="caret"></span></div>
+								<a className="dropdown-toggle hvr-float" data-toggle="dropdown">Build
+								<span className="caret"></span></a>
 								<ul className="dropdown-menu">
 									{this.renderClasses(classes)}
 								</ul>
